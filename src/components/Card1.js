6d1c47549753a6f1b5f504c1dcc359a18c4a4cda
Sai Nandan <agsnandan2004@gmail.com>
2024-03-23 12:57:10 +0530

    landing

--- src/components/Card1.js
+++ src/components/Card1.js
@@ -1,31 +1,24 @@
-import React from 'react'
-import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
-import CardMedia from '@mui/material/CardMedia';
-import CardActions from '@mui/material/CardActions';
-import CallMissedOutgoingIcon from '@mui/icons-material/CallMissedOutgoing';
-import IconButton from '@mui/material/IconButton';
+import React from 'react';
+import { Box, Image, Text, IconButton } from '@chakra-ui/react';
+import { MdCallMissedOutgoing } from 'react-icons/md';
 
 function Card1({ title, subtitle, img }) {
     return (
-
-        <Card sx={{ maxWidth: 345 }}>
-            <CardHeader
-                title={title}
-                subheader={subtitle}
-            />
-            <CardMedia
-                component="img"
-                image={img}
-                alt="Paella dish"
-            />
-            <CardActions disableSpacing>
-                <IconButton aria-label="add to favorites">
-                    <CallMissedOutgoingIcon color='success' />
-                </IconButton>
-            </CardActions>
-        </Card>
-    )
+        <Box className='border p-2 m-3 bg-white border-[#ccc] rounded-md' width="355px" borderWidth="1px" borderRadius="lg" overflow="hidden">
+            <Image src={img} alt="Food" height="150px" objectFit="cover" />
+            <Box p="6">
+                <Box mt="1" fontWeight="semibold" as="h4" lineHeight="tight" isTruncated>
+                    {title}
+                </Box>
+                <Text mt="2" color="gray.600" fontSize="sm">
+                    {subtitle}
+                </Text>
+            </Box>
+            <Box px="6" pb="6">
+                <IconButton aria-label="Add to favorites" icon={<MdCallMissedOutgoing />} colorScheme="green" />
+            </Box>
+        </Box>
+    );
 }
 
-export default Card1
+export default Card1;
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,43 +1,79 @@
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import image1 from '../images/1.jpeg'
-import image2 from '../images/2.jpeg'
-import image3 from '../images/3.jpeg'
-import image4 from '../images/4.jpeg'
-import image5 from '../images/5.jpeg'
-import image6 from '../images/6.jpeg'
-import './landing.css'
-import IconButton from '@mui/material/IconButton';
+import React, { useState } from 'react';
 import Card1 from './Card1';
+import './landing.css'
 
-
-const ExpandMore = styled((props) => {
-    const { expand, ...other } = props;
-    return <IconButton {...other} />;
-})(({ theme, expand }) => ({
-    transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
-    marginLeft: 'auto',
-    transition: theme.transitions.create('transform', {
-        duration: theme.transitions.duration.shortest,
-    }),
-}));
+import image1 from '../images/1.jpeg';
+import image2 from '../images/2.jpeg';
+import image3 from '../images/3.jpeg';
+import image4 from '../images/4.jpeg';
+import image5 from '../images/5.jpeg';
+import image6 from '../images/6.jpeg';
+import { Button, Card, CardBody, CardFooter, Heading, Image, Stack, Text } from '@chakra-ui/react';
+import { NavLink } from 'react-router-dom';
 
 export default function Landing() {
-    const [expanded, setExpanded] = React.useState(false);
+    const [expanded, setExpanded] = useState(false);
 
     const handleExpandClick = () => {
         setExpanded(!expanded);
     };
 
     return (
-        <div className='main'>
-            <Card1 title={"Know your Food"} subtitle={""} img={image1} />
-            <Card1 title={"Fitness Training"} subtitle={""} img={image2} />
-            <Card1 title={"Track your calories"} subtitle={""} img={image3} />
-            <Card1 title={"Integrated Chat Bot"} subtitle={""} img={image4} />
-            <Card1 title={"Monitor your progress on the Dashboard"} subtitle={""} img={image5} />
-            <Card1 title={"Personalized Food Recommendations"} subtitle={""} img={image6} />
+        <div className='bg-white'>
+            <div className='p-10 container-fluid mx-6'>
 
+
+                <Card className='rounded-md mb-10'
+                    direction={{ base: 'column', sm: 'row' }}
+                    overflow='hidden'
+                    variant='outline'
+                >
+                    <Image
+                        objectFit='cover'
+                        src='https://img.freepik.com/free-vector/fitness-concept-illustration_1284-61329.jpg'
+                        alt='fitness'
+                    />
+
+                    <Stack >
+                        <CardBody>
+                            <Heading size='md'>Welcome to fitness tracker</Heading>
+                            <Text py='2'>
+                                Empower your fitness journey with our all-in-one solution - track, set, and achieve your goals effortlessly.
+                            </Text>
+                        </CardBody>
+
+                        <CardFooter>
+                            <NavLink to="/signup">
+                                <Button variant='solid' colorScheme='blue' >
+                                    Get Started
+                                </Button>
+                            </NavLink>
+                        </CardFooter>
+                    </Stack>
+                </Card>
+                <div className="card-container flex justify-between mb-2">
+                    <NavLink to="/nutrition">
+                    <Card1 title={"Know your Food"} subtitle={""} img={image1} />
+                    </NavLink>
+                    <NavLink to="/exercise">
+                    <Card1 title={"Fitness Training"} subtitle={""} img={image2} />
+                    </NavLink>
+                    <NavLink to="/track-calories">
+                    <Card1 title={"Track your calories"} subtitle={""} img={image3} />
+                    </NavLink>
+                </div>
+                <div className="card-container flex justify-between">
+                    <NavLink to="/">
+                    <Card1 title={"Integrated Chat Bot"} subtitle={""} img={image4} />
+                    </NavLink>
+                    <NavLink to="/dashboard">
+                    <Card1 title={"Monitor your progress on the Dashboard"} subtitle={""} img={image5} />
+                    </NavLink>
+                    <NavLink to="/recommendations">
+                    <Card1 title={"Personalized Food Recommendations"} subtitle={""} img={image6} />
+                    </NavLink>
+                </div>
+            </div>
         </div>
     );
 }
